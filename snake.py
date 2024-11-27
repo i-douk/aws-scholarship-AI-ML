b9b14a2bd951f4a606c3102e6fda818c412db2da
i-douk <101892232+i-douk@users.noreply.github.com>
2024-11-27 22:34:07 +0100

    6.9

--- snake.py
+++ snake.py
@@ -1,16 +1,12 @@
-how_many_snakes = 1
-snake_string = """
-Welcome to Python3!
+names =  ['Matt O' , 'Hoyam' , 'Sayed']
+assignments =  [ 0,2,3]
+grades =  [12,14,19]
 
-             ____
-            / . .\\
-            \  ---<
-             \  /
-   __________/ /
--=:___________/
+## message string to be used for each student
+## HINT: use .format() with this string in your for loop
+for x in range(3):
+    print('Hi {},\n\nThis is a reminder that you have {} assignments left to \
+    submit before you can graduate. Your current grade is {} and can increase \
+    to {} if you submit all assignments before the due date.\n\n'.format(names[x],assignments[x],grades[x], 20))
 
-<3, Juno
-"""
-
-
-print(snake_string * how_many_snakes)
+## write a for loop that iterates through each set of names, assignments, and grades to print each student's message
