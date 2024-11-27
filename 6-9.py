2d2ed3d25c342d2741e9f069c97c0b98a7996f82
i-douk <101892232+i-douk@users.noreply.github.com>
2024-11-27 23:36:41 +0100

    cleaned up ex

--- 6-9.py
+++ 6-9.py
@@ -1,12 +1,12 @@
 names =  ['Matt O' , 'Hoyam' , 'Sayed']
 assignments =  [ 0,2,3]
-grades =  [12,14,19]
+grades =  [12,14,18]
 
 ## message string to be used for each student
 ## HINT: use .format() with this string in your for loop
 for x in range(3):
     print('Hi {},\n\nThis is a reminder that you have {} assignments left to \
     submit before you can graduate. Your current grade is {} and can increase \
-    to {} if you submit all assignments before the due date.\n\n'.format(names[x],assignments[x],grades[x], 20))
+    to {} if you submit all assignments before the due date.\n\n'.format(names[x],assignments[x],grades[x], grades[x]+2))
 
 ## write a for loop that iterates through each set of names, assignments, and grades to print each student's message
